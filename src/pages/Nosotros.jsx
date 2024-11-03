a811a28b7a01f23ff21177121a997a2f8c8347fa
Yaslin <1001.29561929.ucla@gmail.com>
2024-11-03 01:29:45 -0400

    contacto.css, faltan detalles

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -43,24 +43,24 @@ export default function Nosotros ()  {
                 <div className='objetivos_content'>
                 <h1 className='objetivos_title'>Objetivos</h1>  
                
-                <p> Brindar a los estudiantes universitarios un servicio eficiente y 
+                <p className='cont'> Brindar a los estudiantes universitarios un servicio eficiente y 
                   de calidad en la gestión de sus recursos financieros, a través de 
                   nuestros canales digitales y nuestros puntos de atención presencial. </p>
 
 
-                  <p> Fomentar el uso de nuestras plataformas digitales para hacer transferencias 
+                  <p className='cont'> Fomentar el uso de nuestras plataformas digitales para hacer transferencias 
                     entre estudiantes sin comisión, depósitos y retiros en efectivo, ofreciendo 
                     herramientas tecnológicas fáciles y seguras.</p>
 
-                  <p> Promover la educación financiera de los estudiantes universitarios, 
+                  <p className='cont'> Promover la educación financiera de los estudiantes universitarios, 
                     a través de charlas, talleres y capacitaciones sobre temas como 
                     el ahorro, la inversión y el uso responsable del crédito.</p>
 
-                     <p>Establecer alianzas estratégicas con universidades y empresas para ofrecer
+                     <p className='cont'>Establecer alianzas estratégicas con universidades y empresas para ofrecer
                      beneficios exclusivos a nuestros clientes, tales como descuentos
                      en matrículas, becas, prácticas laborales, entre otros.</p>
 
-                      <p> Mantener una cultura de innovación y mejora continua en
+                      <p className='cont'> Mantener una cultura de innovación y mejora continua en
                        nuestros procesos, productos y servicios, para estar 
                       siempre a la vanguardia de las necesidades de nuestros
                       clientes y del mercado.</p>
