0db9dd4c9f57ab87a3e6ebc0466160e46fa2b183
Yaslin <1001.29561929.ucla@gmail.com>
2024-11-03 17:36:05 -0400

    Lista pagina contactos. falta responsive

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -28,36 +28,42 @@ export default function Contacto () {
                       
                       <div className='contactos_info'>
                         <div className="contactos">
-                        <h6>Teléfono</h6> 
-                          <p> +58 212 555-5555</p>                         
+                            <h6>Teléfono</h6> 
+                            <p> +58 212 555-5555</p>                                                                           
                         </div>  
                         <img className='img_contacto'
-                        src={soporte}/> 
-                                            
-
+                            src={soporte}/> 
+                        </div>
+                                                                    
+                      <div className='contactos_info'>
                         <div className="contactos">
                         <h6>Fax</h6> 
                           <p> +58 212 555-5555</p>                         
                         </div>
                         <img src={fax} className='img_contacto'/>
+                        </div>
 
+                        <div className='contactos_info'>
                         <div className="contactos">
                         <h6>Correo Electrónico</h6> 
                           <p > info@bancouniversitario.com.ve</p>
                         </div>
                         <img src={gmail} className='img_contacto'/>
+                        </div>
 
+                        <div className='contactos_info'>
                         <div className='contactos'>
                         <h6>Redes Sociales</h6>
                         </div>    
 
-                          <p className= "col-12 col-md-12 center-text"> @bancouniversitariove</p>
+                          
                       </div>   
 
+                      <p className= "rrss"> @bancouniversitariove</p>
                       <div className="col-6 col-md-12 image-container center-text">
-                            <img src={faceimag} className='center-text'/>
-                            <img src={twitter} className='center-text'/>
-                            <img src={instagram} className='center-text'/>
+                            <img src={faceimag} className='redes_icons'/>
+                            <img src={twitter} className='redes_icons'/>
+                            <img src={instagram} className='redes_icons'/>
                           </div>    
                 </div>
                     
