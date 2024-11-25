28610132251f41b8f97ee9735f8655e4208c12f8
Yaslin <1001.29561929.ucla@gmail.com>
2024-11-25 00:27:36 -0400

    boton de ingresar funcional

---
+++ src/pages/Application/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import './Welcome.css'
+
+
+const Welcome=()=>{
+
+    return(
+
+        <div className="principal-container">
+            
+        </div>
+
+    )
+
+
+
+}
