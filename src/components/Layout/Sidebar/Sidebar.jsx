5a39383d6677126d148d48308043e7beb2aa34ed
Ehernandez <edgarht@hotmail.com>
2024-11-25 22:01:55 -0400

    nuevas modificaiones en sidebar y navbar

--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './Sidebar.css'
 import { Link } from "react-router-dom";
+import contac from '../../../assets/images/contacApp.png';
+import movi from '../../../assets/images/moviApp.png';
+import tranf from '../../../assets/images/transfApp.png';
 
 
 
@@ -9,14 +12,16 @@ const Sidebar= () =>{
     return(
         
         <div className="Sidebar">
+      
+            <br />
             <ul>
-                <li><Link to="#">Movimientos</Link></li>
+                <li><Link to="#"><img className='contacApp.png' src={contac}/> Movimientos</Link></li>
             </ul>
             <ul>
-                <li><Link to="#">Transferencia</Link></li>
+                <li><Link to="#"><img className='moviApp.png' src={movi}/>Transferencia</Link></li>
             </ul>
             <ul>
-                <li><Link to="#">Contactos</Link></li>
+                <li><Link to="#"><img className='transfApp.png.png' src={tranf}/>Contactos</Link></li>
             </ul>
         
 
