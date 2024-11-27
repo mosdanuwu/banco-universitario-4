25b1b9cdba57da9cd35bf3c3148ee70c96dd0a1f
Ehernandez <edgarht@hotmail.com>
2024-11-27 15:28:36 -0400

    redireccionamiento funcional y otros detalles

---
+++ src/pages/Application/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavbarApp from '../../components/Layout/NavbarApp/NavbarApp';
+import Sidebar from "../../components/Layout/Sidebar/Sidebar";
+import './Profile.css';
+
+
+ function Profile () {
+  return (
+    
+          <div  className= "prolife">
+             
+             <NavbarApp/>
+             <Sidebar/>
+
+
+           </div>
+              
+
+  
+  )
+}
+export default Profile;
