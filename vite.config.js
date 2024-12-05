56797320c897041b931087cffe430dce8a139af7
mosdanuwu <168379955+mosdanuwu@users.noreply.github.com>
2024-12-05 10:48:28 -0400

    se reconfiguro el vite config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [react()],
   server: { //port: 5173, 
   proxy: { 
@@ -13,4 +13,12 @@ export default defineConfig({
     } 
   } 
 }
+})*/
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 8080
+  }
 })
