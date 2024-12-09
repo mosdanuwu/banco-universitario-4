fe8d8285e39aff4ed840d997f25637e1a0386226
Susam23 <solopararegistros47@gmail.com>
2024-12-08 20:23:14 -0400

    agregue estructura modal a pag contactos

--- src/pages/Application/Contact-Mg.jsx
+++ src/pages/Application/Contact-Mg.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavbarApp from '../../components/Layout/NavbarApp/NavbarApp';
 import Sidebar from "../../components/Layout/Sidebar/Sidebar";
+import buscar from "../../assets/images/Buscar.png";
+import Modal from "../../components/Modal/Modal";
 
 
 import './Contact-Mg.css';
@@ -41,11 +43,11 @@ import './Contact-Mg.css';
                                 
               </div>
                   <div class=" barra input-group mb-3">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Buscar</button>
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon1"><img src={buscar} className="buscar" /></button>
                     <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"/>
                   </div>
                   <div class="button input-group mb-3">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Agregar</button>
+                    <button class="btn btn-primary"><link rel="stylesheet" href="/components/Modal" />Agregar</button>
 
                   </div>
 
